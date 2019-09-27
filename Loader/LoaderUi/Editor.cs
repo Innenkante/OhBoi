bd192062858169a7e18720c29051fd71ffd79463
Mark <chemnitz@arnsperger.org>
2019-09-27 14:00:10 +0200

    Added the new keywords, modied the todo, cleaned up the keywords in the loader, modified the ignored filed.

--- Loader/LoaderUi/Editor.cs
+++ Loader/LoaderUi/Editor.cs
@@ -8,101 +8,12 @@ namespace LoaderUi
 {
     class Editor
     {
-        class Keyword
-        {
-            public int Category { get; set; }
-            public string Word { get; set; }
-            public bool IsMember { get; set; }
-        }
-
-        class DynamicCollection : IEnumerable<AutocompleteMenuNS.AutocompleteItem>
-        {
-            private ScintillaNET.Scintilla _tb;
-            private List<Keyword> _keywords;
-            private List<string> _words = new List<string>();
-
-            private bool IsWordRelevant(string word)
-            {
-                _tb.TargetStart = 0;
-                _tb.TargetEnd = _tb.TextLength;
-                _tb.SearchFlags = SearchFlags.WholeWord;
-
-                if (_tb.SearchInTarget(word) != -1)
-                    return true;
-                else
-                    return false;
-            }
-
-            public void RefreshWords()
-            {
-                _words.RemoveAll(s => !IsWordRelevant(s));
-            }
-
-            public void AddWord(string word)
-            {
-                if (_keywords.Find(k => k.Word == word) == null && !_words.Contains(word))
-                    _words.Add(word);
-            }
-
-            public DynamicCollection(ScintillaNET.Scintilla tb, List<Keyword> source)
-            {
-                _tb = tb;
-                _keywords = source;
-
-                _tb.WordChars = _tb.WordChars + ".";
-            }
-
-            public IEnumerator<AutocompleteMenuNS.AutocompleteItem> GetEnumerator()
-            {
-                return BuildList().GetEnumerator();
-            }
-
-            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-            {
-                return GetEnumerator();
-            }
-
-            private IEnumerable<AutocompleteMenuNS.AutocompleteItem> BuildList()
-            {
-                var startPosition = _tb.WordStartPosition(_tb.CurrentPosition, false);
-
-                var currentWord = _tb.GetWordFromPosition(startPosition);
-
-                if (currentWord.Contains("."))
-                {
-                    foreach (var word in _keywords.Where(k => k.Word.StartsWith(currentWord)))// || k.Word.StartsWith("any.")))
-                        yield return new AutocompleteMenuNS.MethodAutocompleteItem(word.Word.Split('.')[1]);
-
-                    if (_words.Contains(currentWord.Trim('.')))
-                        foreach (var word in _keywords.Where(k => k.Word.StartsWith("any.")))
-                            yield return new AutocompleteMenuNS.MethodAutocompleteItem(word.Word.Split('.')[1]);
-
-                }
-                else
-                {
-                    foreach (var word in _keywords.Where(k => k.Word.StartsWith(currentWord) && !k.Word.Contains(".")))
-                    {
-                        yield return new AutocompleteMenuNS.AutocompleteItem(word.Word);
-                    }
-
-                    foreach(var word in _words.Where(w => w.StartsWith(currentWord)))
-                    {
-                        yield return new AutocompleteMenuNS.AutocompleteItem(word);
-                    }
-                }
-            }
-
-
-        }
-
-
-
         private ScintillaNET.Scintilla _codeArea;
         private AutocompleteMenuNS.AutocompleteMenu _autocompleteMenu;
 
 
         private string _keywordFile = "langDef.json";
-        private DynamicCollection _autocompleteItems;
+        private KeywordCollection _autocompleteItems;
 
         public Editor(ScintillaNET.Scintilla editor, AutocompleteMenuNS.AutocompleteMenu autocomplete)
         {
@@ -254,7 +165,7 @@ namespace LoaderUi
             _codeArea.SetKeywords(1, String.Join(" ", keywords.Where(k => k.Category == 1).Select(k => k.Word.Split('.')[1])));
             _codeArea.SetKeywords(3, String.Join(" ", keywords.Where(k => k.Category == 3).Select(k => k.IsMember ? k.Word.Split('.')[1] : k.Word)));
 
-            _autocompleteItems = new DynamicCollection(_codeArea, keywords);
+            _autocompleteItems = new KeywordCollection(_codeArea, keywords);
 
             _autocompleteMenu.SetAutocompleteItems(_autocompleteItems);
         }
---
+++ Loader/LoaderUi/Keywords.cs
@@ -0,0 +1,93 @@
+﻿using ScintillaNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LoaderUi
+{
+    class Keyword
+    {
+        public int Category { get; set; }
+        public string Word { get; set; }
+        public bool IsMember { get; set; }
+    }
+
+
+    class KeywordCollection : IEnumerable<AutocompleteMenuNS.AutocompleteItem>
+    {
+        private ScintillaNET.Scintilla _tb;
+        private List<Keyword> _keywords;
+        private List<string> _words = new List<string>();
+
+        private bool IsWordRelevant(string word)
+        {
+            _tb.TargetStart = 0;
+            _tb.TargetEnd = _tb.TextLength;
+            _tb.SearchFlags = SearchFlags.WholeWord;
+
+            if (_tb.SearchInTarget(word) != -1)
+                return true;
+            else
+                return false;
+        }
+
+        public void RefreshWords()
+        {
+            _words.RemoveAll(s => !IsWordRelevant(s));
+        }
+
+        public void AddWord(string word)
+        {
+            if (_keywords.Find(k => k.Word == word) == null && !_words.Contains(word))
+                _words.Add(word);
+        }
+
+        public KeywordCollection(Scintilla tb, List<Keyword> source)
+        {
+            _tb = tb;
+            _keywords = source;
+
+            _tb.WordChars = _tb.WordChars + ".";
+        }
+
+        public IEnumerator<AutocompleteMenuNS.AutocompleteItem> GetEnumerator()
+        {
+            return BuildList().GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private IEnumerable<AutocompleteMenuNS.AutocompleteItem> BuildList()
+        {
+            var startPosition = _tb.WordStartPosition(_tb.CurrentPosition, false);
+
+            var currentWord = _tb.GetWordFromPosition(startPosition);
+
+            if (currentWord.Contains("."))
+            {
+                foreach (var word in _keywords.Where(k => k.Word.StartsWith(currentWord)))// || k.Word.StartsWith("any.")))
+                    yield return new AutocompleteMenuNS.MethodAutocompleteItem(word.Word.Split('.')[1]);
+
+                if (_words.Contains(currentWord.Trim('.')))
+                    foreach (var word in _keywords.Where(k => k.Word.StartsWith("any.")))
+                        yield return new AutocompleteMenuNS.MethodAutocompleteItem(word.Word.Split('.')[1]);
+
+            }
+            else
+            {
+                foreach (var word in _keywords.Where(k => k.Word.StartsWith(currentWord) && !k.Word.Contains(".")))
+                {
+                    yield return new AutocompleteMenuNS.AutocompleteItem(word.Word);
+                }
+
+                foreach (var word in _words.Where(w => w.StartsWith(currentWord)))
+                {
+                    yield return new AutocompleteMenuNS.AutocompleteItem(word);
+                }
+            }
+        }
+    }
+}
